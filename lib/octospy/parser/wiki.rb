9e1c37c302c05ed73e52cf1a1a2d2457a4e29bef
linyows <linyows@gmail.com>
2014-03-07 16:33:48 +0900

    fix url of wiki

--- lib/octospy/parser/wiki.rb
+++ lib/octospy/parser/wiki.rb
@@ -5,8 +5,8 @@ module Octospy
         action = @event.payload.pages[0].action
         title = @event.payload.pages[0].title
         sha = @event.payload.pages[0].sha[0,6]
-        url = @event.payload.pages[0].html_url
-        url = "#{url}/_compare/#{sha}%5E...#{sha}" if action == 'edited'
+        url = "#{Octokit.web_endpoint}#{@event.repo.name}/wiki"
+        url += "/#{title}/_compare/#{sha}" if action == 'edited'
 
         {
           status: "#{action} the #{@event.repo.name} wiki",
